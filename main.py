c87410eebd6d38b4552734c48c419c393634ad8f
jong980812 <jong980812@gmail.com>
2022-12-20 20:13:11 +0900

    Add argument print

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import argparse
 import os
 import cv2
+import utils
 from preprocessing import Convert_gray
 
 def get_args():
     parser = argparse.ArgumentParser('VideoMAE fine-tuning and evaluation script for video classification', add_help=False)
-    parser.add_argument('--run_dir', default='./', type=str,                               help='Dir python file' )
+    parser.add_argument('--image_path',default=None,type=str,                               help='Set img path')
+    parser.add_argument('--run_dir', default='./', type=str,                                help='Dir python file' )
     parser.add_argument('--smoothing', default=None, choices=['gaussian', 'avg'], type=str, help='Choice Smoothing method')
     parser.add_argument('--sigma', default=1.0, type=float,                                 help='Set Standard deviation')
-    parser.add_argument('--kernel_size', default=None, type=int,                               help='Direct setting Kernel size')
+    parser.add_argument('--kernel_size', default=None, type=int,                            help='Direct setting Kernel size')
     parser.add_argument('--epsilon', default=0.01, type=float,                              help='Set Threshold Value to determine Kernel size')
     parser.add_argument('--gradient', default='sobel', 
                        choices=['roberts','sobel','prewitt','LoG','DoG'],                   help='Choose Mehotd to calculate gradient')    
@@ -17,8 +19,7 @@ def get_args():
     return parser.parse_args()
 
 def main(args):
-    print(args)
-    img = cv2.imread('/Users/jongseo/Desktop/samples/다운로드.jpeg')
+    img = cv2.imread(args.image_path)
     gray_img=Convert_gray(img)
 
 
@@ -32,7 +33,5 @@ if __name__== "__main__":
         '''
         output_path=os.path.join(args.run_dir,'result')
         os.mkdir(output_path)
-        main(args)
-    else:
-        main(args)
-        
+    utils.arg_print(args)
+    main(args)
